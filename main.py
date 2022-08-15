9c67437e36673d692a0e337480686bb88f6aadc8
RajaFaizanNazir <RFaizanN@gmail.com>
2022-08-15 18:20:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import requests
 url = "https://deep-index.moralis.io/api/v2/ipfs/uploadFolder"
 finalArray = []
+print("Processing")
 for i in os.listdir("images"):
     with open("images/" + str(i), "rb") as image_file:
         b64 = base64.b64encode(image_file.read())
@@ -13,5 +14,7 @@ for i in os.listdir("images"):
 header = {"X-API-Key": "hfIwtLH5S62AGzhPrUMWn6tVddAohLT0nIsCMkdilQYd6DROJfL6k0sdYSFxn3z3",
                   "Content-Type": "application/json; charset=utf-8", "Host": "deep-index.moralis.io",
                   "Content-Length": str(len(json.dumps(body)))}
+print("Uploading")
 responseData = requests.post(url=url, headers=header, data=json.dumps(finalArray))
+print("Uploaded")
 print("BASE URL:"+"/".join(responseData.json()[0]["path"].split("/")[:-1]))
