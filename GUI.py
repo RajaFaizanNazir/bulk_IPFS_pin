033f17461b46a12057b98687485d556645779541
RajaFaizanNazir <RFaizanN@gmail.com>
2022-08-17 11:44:39 +0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,10 +1,13 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, messagebox
 import tkinter as tk
 import json
 
+path = tk.StringVar()
+key = tk.StringVar()
 
-def update_env(path, key):
+
+def update_env():
     json_read = open("constants/env.json", "r")
     env = json.loads(json_read.read())
     json_read.close()
@@ -13,6 +16,7 @@ def update_env(path, key):
     json_write = open("constants/env.json", "w")
     json_write.write(json.dumps(env))
     json_write.close()
+    messagebox.showinfo("Message", "Constant Written")
 
 
 root = Tk()
@@ -20,11 +24,10 @@ frm = ttk.Frame(root, padding=10)
 frm.grid()
 ttk.Label(frm, text="").grid(column=1, row=0)
 ttk.Label(frm, text="Enter absolute path of data folder! ").grid(column=0, row=1)
-path = tk.StringVar()
+
 ttk.Entry(frm, textvariable=path).grid(column=1, row=1)
 ttk.Label(frm, text="Enter X-API-Key of Moralis! ").grid(column=0, row=2)
-key = tk.StringVar()
 ttk.Entry(frm, textvariable=key).grid(column=1, row=2)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=3, row=3)
-ttk.Button(frm, text="Save", command=update_env("#323", "23323")).grid(column=0, row=3)
+ttk.Button(frm, text="Save", command=update_env).grid(column=0, row=3)
 root.mainloop()
